887be6faf95ed0b5ccd02e8c8177501566966cec
NAPO-cyber <gopalverma2503@gmail.com>
2026-08-20 05:05:35 +0530

    prog file

--- src/main/java/scanner/PortProgress.java
+++ src/main/java/scanner/PortProgress.java
@@ -4,11 +4,10 @@ public class PortProgress {
 
     public static void showProgress(int current, int total) {
         int percent = (current * 100) / total;
-        System.out.print("\rProgress: " + percent + "% ");
+        System.out.print("\rProgress: " + percent + "%");
     }
 
     public static void complete() {
-//        System.out.println("\nProgress: 100% - ");
-        System.out.println("\nScan complete.");
+        System.out.println("Scan complete.");
     }
 }
